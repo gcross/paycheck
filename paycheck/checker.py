3af61527118f181c9a9ef6f4beb5daf058cc7676
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-09-27 14:28:54 +1000

    Added @with_checker option to disable the Exception showing the arguments.

--- paycheck/checker.py
+++ paycheck/checker.py
@@ -16,6 +16,7 @@ class Checker(object):
     
     def __init__(self, *args, **keywords):
         self._number_of_calls = keywords.pop('number_of_calls', 100)
+        self._throw_arguments_exception = keywords.pop('throw_arguments_exception', True)
         self._verbose = keywords.pop('verbose', False) 
         self._argument_generators = [PayCheckGenerator.get(t) for t in args]
         self._keyword_generators = [izip(repeat(name),PayCheckGenerator.get(t)) for (name,t) in keywords.iteritems()]
@@ -32,13 +33,19 @@ class Checker(object):
         def wrapper(*pre_args):
             i = 0
             for (args,keywords) in generator:
+                if self._verbose:
+                    sys.stderr.write("%d: %r\n" % (i, args))
                 try:
-                    if self._verbose:
-                        sys.stderr.write("%d: %r\n" % (i, args))
                     test_func(*(pre_args+args), **dict(keywords))
                 except Exception, e:
+                    if not self._throw_arguments_exception:
+                        raise
                     if sys.version_info[0] < 3:
-                        raise e.__class__("Failed for input %s with message '%s'" % (args+keywords,e)), None, sys.exc_traceback
+                        try:
+                            new_e = e.__class__("Failed for input %s with message '%s'" % (args+keywords,e))
+                        except:
+                            new_e = e
+                        raise new_e, None, sys.exc_traceback
                     else:
                         raise Exception("Failed for input {}".format(args))
                 i += 1
